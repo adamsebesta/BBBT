3dd8f8c31a9930af38cbcff35fb692a4567df191
Adam Sebesta <adam.sebesta5@gmail.com>
2020-07-16 12:12:50 +0200

    added routes for clients collection

--- app/models/client.model.js
+++ app/models/client.model.js
@@ -1,9 +1,9 @@
-// const mongoose = require('mongoose');
-//
-// const ClientSchema = new Schema({
-//     name: String
-// },
-//   { timestamps: true }
-// )
-//
-// module.exports = mongoose.model('Client', ClientSchema);
+const mongoose = require('mongoose');
+
+const ClientSchema = mongoose.Schema({
+    name: String
+    
+}, { timestamps: true }
+)
+
+module.exports = mongoose.model('Client', ClientSchema);
